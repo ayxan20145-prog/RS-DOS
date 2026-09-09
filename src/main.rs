a064d38d9006ad587d42ed2a382a04a97e3ed639
ayxan <ayxan.20145@gmail.com>
2026-09-09 11:12:43 +0400

    added keyboard but i get a kernel panic for index out of bounds

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,17 @@ mod panic;
 mod vga;
 
 use core::{arch::global_asm, fmt::Write};
+use x86::io::inb;
 
 global_asm!(include_str!("boot.asm"));
+
+const KEYMAP: [Option<char>; 128] = {
+    let mut map = [None; 128];
+    map[0x02] = Some('1');
+    map[0x03] = Some('2');
+
+    map
+};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn kernel_main() -> ! {
@@ -18,5 +27,24 @@ pub extern "C" fn kernel_main() -> ! {
     )
     .unwrap();
     write!(writer, "\nC:\\>").unwrap();
-    loop {}
+    loop {
+        match read_key() {
+            Some(key) => write!(writer, "{}", key).unwrap(),
+            None => {}
+        }
+    }
+}
+
+fn read_scancode() -> u8 {
+    unsafe {
+        while (inb(0x64) & 1) == 0 {
+            continue;
+        }
+        inb(0x60)
+    }
+}
+
+fn read_key() -> Option<char> {
+    let scancode = read_scancode();
+    KEYMAP[scancode as usize]
 }
