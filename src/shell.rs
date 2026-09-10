80510520b60d9c2993d2c6f9b4db5190f52379b7
ayxan <ayxan.20145@gmail.com>
2026-09-10 12:08:53 +0400

    made commands functions

--- src/shell.rs
+++ src/shell.rs
@@ -20,30 +20,22 @@ pub fn run(writer: &mut Writer) {
                         write!(writer, "\n\nC:\\>").unwrap();
                     } else {
                         if cmd_len == 4 && &cmd_buffer[..4] == b"help" {
-                            write!(writer, "\nhelp\ncls\necho\nver\nhalt\npanic").unwrap();
+                            cmd_help(writer);
                             write!(writer, "\nC:\\>").unwrap();
                         } else if cmd_len == 3 && &cmd_buffer[..3] == b"cls" {
-                            writer.clear(0x0F);
-                            writer.reset_cursor();
+                            cmd_cls(writer);
                             write!(writer, "C:\\>").unwrap();
                         } else if cmd_len >= 4 && &cmd_buffer[..4] == b"echo" {
-                            if cmd_len == 4 {
-                                write!(writer, "\n").unwrap();
-                            } else {
-                                let start = 5;
-                                let arg =
-                                    core::str::from_utf8(&cmd_buffer[start..cmd_len]).unwrap_or("");
-                                write!(writer, "\n{}", arg).unwrap();
-                            }
+                            cmd_echo(writer, &cmd_buffer, cmd_len);
                             write!(writer, "\nC:\\>").unwrap();
                         } else if cmd_len == 3 && &cmd_buffer[..3] == b"ver" {
-                            write!(writer, "\n{}", env!("CARGO_PKG_VERSION")).unwrap();
+                            cmd_ver(writer);
                             write!(writer, "\nC:\\>").unwrap();
                         } else if cmd_len == 4 && &cmd_buffer[..4] == b"halt" {
-                            write!(writer, "\nSystem halted").unwrap();
+                            cmd_halt(writer);
                             return;
                         } else if cmd_len == 5 && &cmd_buffer[..5] == b"panic" {
-                            panic!();
+                            cmd_panic();
                         } else {
                             let command = core::str::from_utf8(&cmd_buffer[..cmd_len]).unwrap();
                             write!(writer, "\nunknown command: {}", command).unwrap();
@@ -68,4 +60,29 @@ pub fn run(writer: &mut Writer) {
             }
         }
     }
+}
+fn cmd_help(writer: &mut Writer) {
+    write!(writer, "\nhelp\ncls\necho\nver\nhalt\npanic").unwrap();
+}
+fn cmd_cls(writer: &mut Writer) {
+    writer.clear(0x0F);
+    writer.reset_cursor();
+}
+fn cmd_echo(writer: &mut Writer, cmd_buffer: &[u8], cmd_len: usize) {
+    if cmd_len == 4 {
+        write!(writer, "\n").unwrap();
+    } else {
+        let start = 5;
+        let arg = core::str::from_utf8(&cmd_buffer[start..cmd_len]).unwrap_or("");
+        write!(writer, "\n{}", arg).unwrap();
+    }
+}
+fn cmd_ver(writer: &mut Writer) {
+    write!(writer, "\n{}", env!("CARGO_PKG_VERSION")).unwrap();
+}
+fn cmd_halt(writer: &mut Writer) {
+    write!(writer, "\nSystem halted").unwrap();
+}
+fn cmd_panic() {
+    panic!();
 }
