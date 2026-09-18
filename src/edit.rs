de8d7e4421dc8a4719d3751ce9e2d2f10fcc9616
ayxan <ayxan.20145@gmail.com>
2026-09-18 21:56:46 +0400

    added Editor struct which has buf and buf_len

--- src/edit.rs
+++ src/edit.rs
@@ -1,13 +1,26 @@
 use crate::{keyboard, print, vga::writer};
 use core::fmt::Write;
 
+struct Editor {
+    buf: [u8; 256],
+    buf_len: usize,
+}
 pub fn edit() {
     writer().clear();
     writer().reset_cursor();
 
+    let mut editor = Editor {
+        buf: [0u8; 256],
+        buf_len: 0,
+    };
+
     loop {
         if let Some(key) = keyboard::read_key() {
-            print!("{}", key);
+            if editor.buf_len < 256 {
+                editor.buf[editor.buf_len] = key as u8;
+                editor.buf_len += 1;
+                print!("{}", key);
+            }
         }
     }
 }
