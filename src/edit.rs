0daf9d1fa6b56e07e72aa639d53d8819498efe25
ayxan <ayxan.20145@gmail.com>
2026-09-18 22:16:49 +0400

    added esc key to keyboard and added modes to edit

--- src/edit.rs
+++ src/edit.rs
@@ -1,7 +1,13 @@
 use crate::{keyboard, print, vga::writer};
 use core::fmt::Write;
 
+enum Mode {
+    Normal,
+    Insert,
+}
+
 struct Editor {
+    mode: Mode,
     buf: [u8; 256],
     buf_len: usize,
 }
@@ -10,26 +16,46 @@ pub fn edit() {
     writer().reset_cursor();
 
     let mut editor = Editor {
+        mode: Mode::Normal,
         buf: [0u8; 256],
         buf_len: 0,
     };
 
     loop {
         if let Some(key) = keyboard::read_key() {
-            match key {
-                '\x08' => {
-                    if editor.buf_len > 0 {
-                        editor.buf_len -= 1;
-                        writer().write_byte(b'\x08');
+            match editor.mode {
+                Mode::Normal => match key {
+                    'q' => {
+                        writer().clear();
+                        writer().reset_cursor();
+                        return;
                     }
-                }
-                _ => {
-                    if editor.buf_len < 256 {
-                        editor.buf[editor.buf_len] = key as u8;
-                        editor.buf_len += 1;
-                        print!("{}", key);
+                    'i' => editor.mode = Mode::Insert,
+                    _ => {}
+                },
+                Mode::Insert => match key {
+                    '\n' => {
+                        if editor.buf_len < editor.buf.len() {
+                            editor.buf[editor.buf_len] = b'\n';
+                            editor.buf_len += 1;
+                            writer().write_byte(b'\n');
+                        }
                     }
-                }
+                    '\x08' => {
+                        if editor.buf_len > 0 {
+                            editor.buf_len -= 1;
+                            writer().write_byte(b'\x08');
+                        }
+                    }
+                    '\x1B' => editor.mode = Mode::Normal,
+                    _ => {
+                        if editor.buf_len < 256 {
+                            editor.buf[editor.buf_len] = key as u8;
+                            editor.buf_len += 1;
+                            print!("{}", key);
+                        }
+                    }
+                },
             }
         }
     }
--- src/keyboard.rs
+++ src/keyboard.rs
@@ -59,6 +59,7 @@ pub const KEYMAP: [Option<char>; 128] = {
     map[0x35] = Some('/');
 
     // other keys
+    map[0x01] = Some('\x1B');
     map[0x1c] = Some('\n');
     map[0x39] = Some(' ');
     map[0x0E] = Some('\x08');
